c29eb7faeb13d39608b02e24322c3229f449cbd8
Meriipu <Meriipu@users.noreply.github.com>
2018-08-08 19:00:55 +0200

    minor cleanup

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
-import sys, random
 import numpy as np
 from matplotlib import pyplot as plt
-from dutil import *
+import dutil
 
 NUM_EPOCHS = 50
 BATCH_SIZE = 20
@@ -22,14 +21,13 @@ def plotScores(scores, test_scores, fname, on_top=True):
 	plt.savefig(fname)
 
 #Load data set
-print "Loading Data..."
+print("Loading Data...")
 x_train = np.load('x_data.npy').astype(np.float32) / 255.0
 y_train = np.load('y_data.npy').astype(np.float32) / 255.0
 num_samples = x_train.shape[0]
-print "Loaded " + str(num_samples) + " Samples."
-
-print "Attaching more channels..."
-x_train = add_pos(x_train)
+print("Loaded {} samples".format(num_samples))
+print("Attaching more channels...")
+x_train = dutil.add_pos(x_train)
 
 #Split data
 split_ix = num_samples/VALID_RATIO
@@ -38,7 +36,7 @@ y_test = y_train[:split_ix]
 x_train = x_train[split_ix:]
 y_train = y_train[split_ix:]
 
-print "Shuffling..."
+print("Shuffling...")
 np.random.seed(0)
 rng_state = np.random.get_state()
 np.random.shuffle(x_train)
@@ -50,12 +48,12 @@ y_train_mini = y_train[:len(y_train)/VALID_RATIO]
 ###################################
 #  Create Model
 ###################################
-print "Loading Keras..."
+print("Loading Keras...")
 import os, math
 os.environ['THEANORC'] = "./gpu.theanorc"
 os.environ['KERAS_BACKEND'] = "theano"
 import theano
-print "Theano Version: " + theano.__version__
+print("Theano Version: {}".format(theano.__version__))
 from keras.layers import Input, Dense, Activation, Dropout, Flatten, Reshape, concatenate
 from keras.layers.convolutional import Conv2D, Conv2DTranspose, UpSampling2D
 from keras.layers.local import LocallyConnected2D
@@ -69,11 +67,11 @@ from keras import backend as K
 K.set_image_data_format('channels_first')
 
 if False:
-	print "Loading Model..."
+	print("Loading Model...")
 	model = load_model('Model.h5')
 	model.optimizer.lr.set_value(lr)
 else:
-	print "Building Model..."
+	print("Building Model...")
 	model = Sequential()
 
 	model.add(Conv2D(48, (5, 5), padding='same', input_shape=x_train.shape[1:]))
@@ -105,24 +103,24 @@ else:
 ###################################
 #  Train
 ###################################
-print "Training..."
+print("Training...")
 train_rmse = []
 test_rmse = []
 
-for i in xrange(NUM_EPOCHS):
+for i in range(NUM_EPOCHS):
 	model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=1)
 
 	mse = model.evaluate(x_train_mini, y_train_mini, batch_size=128, verbose=0)
 	train_rmse.append(math.sqrt(mse))
-	print "Train RMSE: " + str(train_rmse[-1])
+	print("Train RMSE: {}".format(train_rmse[-1]))
 
 	mse = model.evaluate(x_test, y_test, batch_size=128, verbose=0)
 	test_rmse.append(math.sqrt(mse))
-	print "Test RMSE: " + str(test_rmse[-1])
+	print("Test RMSE: {}".format(test_rmse[-1]))
 
 	model.save('Model.h5')
-	print "Saved"
+	print("Saved")
 
 	plotScores(train_rmse, test_rmse, 'Scores.png', True)
 
-print "Done"
+print("Done")
